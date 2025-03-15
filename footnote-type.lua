4711f98609413db73397b1a84ed1af5d47c9206f
Fred Guth <fredguth@fredguth.com>
2025-03-15 16:00:34 -0300

    simplify mwe again

--- footnote-type.lua
+++ footnote-type.lua
@@ -1,39 +1,16 @@
-function RawInline(el)
-    -- Look for footnote pattern <fn id="...">
-    local content = el.text
-    content = content:gsub('<fn%s+id="([^"]+)">', '<fn fn-type="other" id="%1">')
-    
-    -- Return the modified element if changes were made
-    if content ~= el.text then
-        el.text = content
-        return el
-    end
 
-    -- Return unchanged if no modifications were needed
+function RawBlock(el)
+    if el.format == "html" then
+      print(':::', el.text)
+      el.text = el.text:gsub('<fn%s+id="([^"]+)">', '<fn fn-type="other" id="%1">')
+      print(':::', el.text)
+    end
     return el
   end
   
-  function RawBlock(el)
 
-    -- Look for footnote pattern <fn id="...">
-    local content = el.text
-    -- Use pattern matching to find and replace the footnote format
-    content = content:gsub('<fn%s+id="([^"]+)">', '<fn fn-type="other" id="%1">')
-    
-    -- Return the modified element if changes were made
-    if content ~= el.text then
-    el.text = content
-    return el
-    end
-
-    -- Return unchanged if no modifications were needed
-    return el
-  end
-
-
-
-
+  
   return {
-    RawInline = RawInline,
-    RawBlock = RawBlock
+    RawBlock = RawBlock,
+    
   }
